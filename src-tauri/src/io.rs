f99b8e4b05abfc50e9e48aa1fa69a5b0b54a3092
saileille <aleksi.halonen@gmail.com>
2025-10-06 21:50:20 +0300

    Changed name directory to be dynamic.

--- src-tauri/src/io.rs
+++ src-tauri/src/io.rs
@@ -7,14 +7,14 @@ fn read_json_file<S: AsRef<str>>(path: S) -> String {
     // Read a JSON file and return it as a string.
     let mut json: String = String::new();
     let mut file: File = File::open(path.as_ref()).unwrap();
-    
+
     file.read_to_string(&mut json).unwrap();
     return json
 }
 
 pub fn load_country_names<S: AsRef<str>>(country: S) -> HashMap<String, HashMap<String, u16>> {
     // Load names of a specific country.
-    let json: String = read_json_file(format!("E:/Tiedostot/koodaus/Tauri/icehockeysim/Ice Hockey Sim/src-tauri/json/names/{}.json", country.as_ref()));
+    let json: String = read_json_file(format!("./json/names/{}.json", country.as_ref()));
     let names: HashMap<String, HashMap<String, u16>> = serde_json::from_str(&json).unwrap();
     return names
 }
@@ -22,9 +22,9 @@ pub fn load_country_names<S: AsRef<str>>(country: S) -> HashMap<String, HashMap<
 // Function for listing all JSON files in the names folder.
 // Used for generating countries in the database.
 pub fn get_countries_from_name_files() -> Vec<String> {
-    let paths: ReadDir = read_dir("E:/Tiedostot/koodaus/Tauri/icehockeysim/Ice Hockey Sim/src-tauri/json/names/").unwrap();
+    let paths: ReadDir = read_dir("./json/names/").unwrap();
     let mut countries: Vec<String> = Vec::new();
-    
+
     for path in paths {
         let filename: String = format!("{}", path.unwrap().file_name().display());
         if !filename.ends_with(".json") { continue }
